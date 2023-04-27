5ce1aa644f5c961961fd89e1f27fe6a6ac4a52a8
Muhammad <87659760+Mu99Ti@users.noreply.github.com>
2023-04-27 16:51:37 +0330

    request has been added.

--- MovieData/Model.swift
+++ MovieData/Model.swift
@@ -5,68 +5,75 @@
 //  Created by Muhamad Talebi on 4/26/23.
 //
 
-import Foundation
+import UIKit
 
-struct Model: Decodable {
-    
-    var page: String?
-    var results: [ResultModel]?
-    var totalPages: String?
-    var totalResults : String?
-    
-    enum AdditionalKeysInfo: String, CodingKey {
-        case page
-        case results
-        case totalPages = "total_pages"
-        case totalResults = "total_results"
-    }
-}
+// URL: https://api.themoviedb.org/3/search/movie?page=1&api_key=852ffe4cfff2346ad60b709d653c5ea3&query=batman
 
-extension Model {
-    struct ResultModel: Decodable {
-       
-        var adult: String?
-        var backdropPath: String?
-        var id: Int?
-        var originalLanguage: Language?
-        var originalTitle: String?
-        var overView: String?
-        var popularity: String?
-        var posterPath: String?
-        var releaseDate: String?
-        var title: String?
-        var video: Bool
-        var voteAverage: CGFloat?
-        var voteCount: Int64?
-        
-        
-        var posterImageURL: URL? {
-            let url = URL(string: "https://image.tmdb.org/t/p/w200\(String(describing: posterPath))")
-                return url
-        }
-     
-        enum AdditionalResultKeysInfo: String, CodingKey {
-            case backdropPath = "backdrop_path"
-            case id
-            case originalLanguage = "original_language"
-            case originalTitle = "original_title"
-            case overView = "overview"
-            case popularity
-            case posterPath = "poster_path"
-            case releaseDate = "release_date"
-            case title
-            case video
-            case voteAverage = "vote_average"
-            case voteCount = "vote_count"
-        }
-    }
-    
-}
-
-extension Model {
-    enum Language: String, Decodable {
-        case english = "en"
-        case persian = "fa"
-    }
-}
-
+//struct Model: Decodable {
+//    
+//    var page: Int
+//    var results: [ResultModel]
+//    var totalPages: Int
+//    var totalResults : Int
+//    
+//    enum CodingKeys: String, CodingKey {
+//        case page
+//        case results
+//        case totalPages = "total_pages"
+//        case totalResults = "total_results"
+//    }
+//}
+//
+//extension Model {
+//    struct ResultModel: Decodable {
+//       
+////        var adult: Bool
+////        var backdropPath: String?
+//        var id: Int
+////        var originalLanguage: String?
+////        var originalTitle: String?
+////        var overView: String?
+////        var popularity: Double?
+//        var posterPath: String?
+//        var releaseDate: String?
+//        var title: String?
+////        var video: Bool
+////        var voteAverage: CGFloat?
+////        var voteCount: Int64?
+//        
+//        
+//        var posterImageURL: URL? {
+//            let url = URL(string: "https://image.tmdb.org/t/p/w200\(String(describing: posterPath))")
+//            return url
+//        }
+//     
+//        enum CodingKeys: String, CodingKey {
+////            case backdropPath = "backdrop_path"
+//            case id
+////            case originalLanguage = "original_language"
+////            case originalTitle = "original_title"
+////            case overView = "overview"
+////            case popularity
+//            case posterPath = "poster_path"
+//            case releaseDate = "release_date"
+//            case title
+////            case video
+////            case adult
+////            case voteAverage = "vote_average"
+////            case voteCount = "vote_count"
+//        }
+//    }
+//}
+//
+//let url = URL(string: "https://api.themoviedb.org/3/search/movie?page=1&api_key=852ffe4cfff2346ad60b709d653c5ea3&query=batman")!
+//let request = URLRequest(url: url)
+//URLSession.shared.dataTask(with: request) { data, response, error in
+//    if let data {
+//        do {
+//            let decoded = try JSONDecoder().decode(Model.self, from: data)
+//            print("The json is successfully decoded:", decoded)
+//        } catch {
+//            print(String(describing: error))
+//        }
+//    }
+//}.resume()
--- MovieData/ViewController.swift
+++ MovieData/ViewController.swift
@@ -38,10 +38,29 @@ class ViewController: UIViewController {
             tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
         ])
     }
+    
+    func fetchMovies(_ keyword: String) {
+        let url = URL(string: "https://api.themoviedb.org/3/search/movie?page=1&api_key=852ffe4cfff2346ad60b709d653c5ea3&query=\(keyword)")!
+        let request = URLRequest(url: url)
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let data {
+                do {
+                    let decoded = try JSONDecoder().decode(Model.self, from: data)
+                    print("The json is successfully decoded:", decoded)
+                } catch {
+                    print(String(describing: error))
+                }
+            }
+        }.resume()
+    }
 }
 
 extension ViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        var keyword = searchBar.searchTextField.text
+        if let keyword {
+            fetchMovies(keyword)
+        }
     }
 }
 
@@ -53,7 +72,6 @@ extension ViewController: UITableViewDelegate {
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 30
-    https://image.tmdb.org/t/p/w200/
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -145,6 +163,64 @@ extension ViewController {
             movieImageView.image = image
             dateLabel.text = date
         }
+        
     }
 }
+
+struct Model: Decodable {
+    
+    var page: Int
+    var results: [ResultModel]
+    var totalPages: Int
+    var totalResults : Int
+    
+    enum CodingKeys: String, CodingKey {
+        case page
+        case results
+        case totalPages = "total_pages"
+        case totalResults = "total_results"
+    }
+}
+
+extension Model {
+    struct ResultModel: Decodable {
+       
+//        var adult: Bool
+//        var backdropPath: String?
+        var id: Int
+//        var originalLanguage: String?
+//        var originalTitle: String?
+//        var overView: String?
+//        var popularity: Double?
+        var posterPath: String?
+        var releaseDate: String?
+        var title: String?
+//        var video: Bool
+//        var voteAverage: CGFloat?
+//        var voteCount: Int64?
+        
+        
+        var posterImageURL: URL? {
+            let url = URL(string: "https://image.tmdb.org/t/p/w200\(String(describing: posterPath))")
+            return url
+        }
+     
+        enum CodingKeys: String, CodingKey {
+//            case backdropPath = "backdrop_path"
+            case id
+//            case originalLanguage = "original_language"
+//            case originalTitle = "original_title"
+//            case overView = "overview"
+//            case popularity
+            case posterPath = "poster_path"
+            case releaseDate = "release_date"
+            case title
+//            case video
+//            case adult
+//            case voteAverage = "vote_average"
+//            case voteCount = "vote_count"
+        }
+    }
+}
+
 
